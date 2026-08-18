8c3944b6ab21a655064dac1f96ee0562e3615c90
Student <Student@LabVM2049939.jv03jwyx44iuve4cb404kghoog.jnbx.internal.cloudapp.net>
2026-08-18 15:29:53 +0200

    Add AcademicReslutEvaluator main class Implement mark validation for Academic Result Evaluator Implement academic result evaluation

--- AcademicResultEvaluator/src/AcademicResultEvaluator.java
+++ AcademicResultEvaluator/src/AcademicResultEvaluator.java
@@ -9,4 +9,22 @@ public class AcademicResultEvaluator {
     public boolean isMarkValid() {
         return mark >= 0 && mark <= 100;
     }
+    public String determineAcademicResult() {
+
+        if (!isMarkValid()) {
+            return "Invalid mark";
+        }
+        if (mark >= 90) {
+            return "Excellent";
+        } else if (mark >= 75) {
+            return "Very Good";
+        } else if (mark >= 60) {
+            return "Good";
+        } else if (mark >= 50) {
+            return "Pass";
+        } else {
+            return "Fail";
+        }
+    }
+
 }
